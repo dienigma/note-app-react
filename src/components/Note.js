4a5c3056e6fbf3516770b9a81a36cb5099c90e04
dienigma <joshi.chinmay09@gmail.com>
2019-10-13 07:55:04 +0530

    Abstracted the custom hook

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,22 +1,6 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import NotesContext from "../context/notes-context";
-
-const useMousePosition = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleMouseMove = e => {
-      setPosition({ x: e.pageX, y: e.pageY });
-    };
-    document.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
-
-  return position;
-};
+import useMousePosition from "../hooks/useMousePosition";
 
 const Note = ({ note }) => {
   const { dispatch } = useContext(NotesContext);
